610b98b18461b8196f8ea14dd63664d142c96034
Naharun <naharunsraboni@gmail.com>
2023-06-10 08:50:52 +0600

    added Registration page

--- src/pages/Registration/Registration.jsx
+++ src/pages/Registration/Registration.jsx
@@ -38,8 +38,7 @@ const Registration = () => {
             <div className="hero min-h-screen bg-base-200">
                 <div className="hero-content flex-col lg:flex-row-reverse">
                     <div className="text-center lg:text-left">
-                        <h1 className="text-5xl font-bold">Sign Up now!</h1>
-                        <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                        <h1 className="text-5xl font-bold">Registration now!</h1>
                     </div>
                     <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
                         <form onSubmit={handleSubmit(onSubmit)} className="card-body">
@@ -83,8 +82,15 @@ const Registration = () => {
                                     <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                                 </label>
                             </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Confirm Password</span>
+                                </label>
+                                <input type="text" {...register("confirm password", { required: true })} name="confirm password" placeholder="confirm password" className="input input-bordered" />
+                                {errors.name && <span className="text-red-700">Name is required</span>}
+                            </div>
                             <div className="form-control mt-6">
-                                <input className="btn btn-primary" type="submit" value="registration" />
+                                <input className="btn btn-neutral" type="submit" value="registration" />
                             </div>
                         </form>
                     </div>

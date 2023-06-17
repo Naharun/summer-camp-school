27e944f119624c3c56b47053010e0641877bfd0d
Naharun <naharunsraboni@gmail.com>
2023-06-17 16:51:44 +0600

    created popular instructor section

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -2,6 +2,7 @@ import { Helmet } from "react-helmet-async";
 import TopSliderSection from "../TopSliderSection/TopSliderSection";
 import PopularClassesSection from "../PopularClassesSection/PopularClassesSection";
 import Review from "../../ExtraSection/Review";
+import PopularInstructors from "./PopularInstructors";
 
 const Home = () => {
     return (
@@ -10,6 +11,7 @@ const Home = () => {
                 <title>Instrument Of Percussion || Home</title>
             </Helmet>
             <TopSliderSection></TopSliderSection>
+            <PopularInstructors></PopularInstructors>
             <Review></Review>
             <PopularClassesSection></PopularClassesSection>
         </div>
---
+++ src/pages/Home/Home/PopularInstructors.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+
+const PopularInstructors = () => {
+    const [instructors, setInstructor] = useState([]);
+    useEffect(()=>{
+        fetch('https://summer-camp-school-server-beige.vercel.app/users/instructor')
+        .then(res=>res.json())
+        .then(data=>setInstructor(data))
+    },[] );
+
+    return (
+        <div className=" py-5 px-4">
+            <h1 className="text-sxl font-bold text-center">Popular Instructors Here</h1>
+            <hr />
+            <div className="grid md:grid-cols-2 lg:grid-cols-3 space-y-4">
+                {
+                    instructors.map(instructor => <div className="card w-96 bg-base-100 shadow-xl py-5" key={instructor._id}>
+                        <figure><img className="w-[320px] h-[200px]" src={instructor.photo} alt="Shoes" /></figure>
+                        <div className="card-body">
+                            <h2 className="card-title font-bold">{instructor.name}</h2>
+                            <p className="text-slate-800">Email: {instructor.email}</p>
+                        </div>
+                    </div>)
+                }
+            </div>
+
+        </div>
+    );
+};
+
+export default PopularInstructors;
